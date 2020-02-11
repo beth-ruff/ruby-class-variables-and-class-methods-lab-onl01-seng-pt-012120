4c17f26fba6bb24e5b50208354a223e061a4fcdd
Beth Ruff <beth.ruff2@gmail.com>
2020-02-11 03:34:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,18 @@ def self.genre_count
   genre_count
 end
 
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1 
+    else
+      artist_count[artist] = 1 
+    end
+  end
+  artist_count
+end
+
 end
 
 hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
