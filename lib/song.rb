8a29be449527449252a77a1936505405ac4948e6
Beth Ruff <beth.ruff2@gmail.com>
2020-02-11 03:20:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,22 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
+  @@artists = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist 
   @genre = genre
   @@count += 1 
+  @@artists << artist
 end
 
 def self.count
   @@count
+end
+
+def self.artists 
+  @@artists 
 end
 
 end
